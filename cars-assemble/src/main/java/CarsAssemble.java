678a1a518bf4b62bb4409c81ab37875bbaadb65d
Bianca Barcelos <biancamariamichels@gmail.com>
2024-01-21 23:33:13 +0000

    cars-assemble

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        int itemsPerHour = 221 * speed;
+
+        if (speed > 0 && speed <= 4) {
+            return itemsPerHour;
+        }
+        if ( speed >= 5 && speed <=8) {
+            return itemsPerHour * 0.9;
+        }
+        if (speed == 9) {
+            return itemsPerHour * 0.8;
+        }
+        if (speed == 10) {
+            return itemsPerHour * 0.77;
+        }
+
+        return 0;
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60);
     }
 }
